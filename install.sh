8b877b9438420a06c0903b2e9c2ea234b6e881c7
Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
2018-11-29 11:47:07 +1300

    Change: use latest version of pharcc

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 if hash pharcc 2>/dev/null; then
     echo "pharcc command found!"
 else
-    wget https://github.com/cbednarski/pharcc/releases/download/v0.2.3/pharcc.phar
+    wget https://github.com/cbednarski/pharcc/releases/download/v0.2.4/pharcc.phar
     chmod +x pharcc.phar
     sudo mv pharcc.phar /usr/local/bin/pharcc
 fi
