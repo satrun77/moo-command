b752f720a16fb46a0db05db35357069504894c8c
Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
2019-03-01 13:47:22 +1300

    Feature: allow to execute command inside a container

---
+++ src/MooCommand/Command/Workspace/Sh.php
@@ -0,0 +1,76 @@
+<?php
+/*
+ * This file is part of the MooCommand package.
+ *
+ * (c) Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace MooCommand\Command\Workspace;
+
+use MooCommand\Command\Workspace as WorkspaceAbstract;
+use Symfony\Component\Console\Input\InputArgument;
+
+/**
+ * Sh execute a command inside a container.
+ *
+ * @author Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
+ */
+class Sh extends WorkspaceAbstract
+{
+    /**
+     * @var string
+     */
+    protected $description = 'Execute a command inside a container for a site within the workspace.';
+    /**
+     * @var string
+     */
+    protected $childSignature = 'sh';
+    /**
+     * @var array
+     */
+    protected $arguments = [
+        'name'      => [
+            'mode'        => InputArgument::REQUIRED,
+            'description' => 'Name of the directory containing the docker/site files',
+        ],
+        'container' => [
+            'mode'        => InputArgument::REQUIRED,
+            'description' => 'Name of the container to execute command into',
+        ],
+        'execute' => [
+            'mode'        => InputArgument::REQUIRED,
+            'description' => 'Command to execute inside the container',
+        ],
+    ];
+
+    /**
+     * Main method to execute the command script.
+     *
+     * @return void
+     *
+     * @throws \Exception
+     */
+    protected function fire()
+    {
+        // Move to root directory of docker setup
+        $this->changeToSiteDirectory();
+
+        // Ensure site name does not include "."
+        $siteName  = str_replace('.', '', $this->argument('name'));
+        // Name of the container from command argument
+        $container = $this->argument('container');
+        // Convert name of the container to the name used by docker
+        $containerName = $container;
+        if ('proxy' !== $siteName) {
+            $containerName = sprintf('%s_%s_1', $siteName, $container);
+        }
+        // Command we want to execute inside container
+        $command = $this->argument('execute');
+
+        // Execute command inside the container
+        $this->getShellHelper()->execRealTime('docker exec -i -t %s %s', $containerName, $command);
+    }
+}
